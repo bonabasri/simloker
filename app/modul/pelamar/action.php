43caf7e299eb0c9cbc56f4a8238adb7db8c7c07f
luqmanhakim1 <luckman.heckem@gmail.com>
2017-06-07 10:02:36 +0700

    update modul

--- app/modul/pelamar/action.php
+++ app/modul/pelamar/action.php
@@ -1,7 +1,7 @@
 <?php
 	if ( isset($_POST['save']) ) 
 	{
-		$allowExt 			= array( 'png', 'jpg' );
+		$allowExt 			= array( 'png', 'jpg', 'jpeg' );
 
 		$fileName 			= $_FILES['file']['name'];
 		$fileExt			= strtolower(end(explode('.', $filename)));
@@ -23,6 +23,64 @@
 		$v_tinggi_badan		= $_POST['tinggi_badan'];
 
 		$v_kelebihan		= $_POST['kelebihan'];
+
+		$upload_dir 		= "../dist/images/foto/";
+		$file 				= basename ($fileName);
+		$v_file 			= str_replace(' ','_',$file);
+
+		if ( in_array( $fileExt, $allowExt ) === TRUE ) 
+		{
+			if ( $fileSize < 1044070 ) 
+			{
+				if ( move_uploaded_file( $fileTemp,$upload_dir.$v_file) ) 
+				{
+					$sql = "INSERT INTO tb_pelamar (
+													nama_depan,
+													nama_belakang,
+													tempat_lahir,
+													tgl_lahir,
+													jk,
+													agama,
+													alamat,
+													lulusan,
+													usia,
+													no_hp,
+													pengalaman,
+													email,
+													tinggi_badan,
+													foto,
+													kelebihan)
+													VALUES (
+															'$v_nama_depan',
+															'$v_nama_belakang',
+															'$v_tempat_lahir',
+															'$v_tgl_lahir',
+															'$v_jk',
+															'$v_agama',
+															'$v_alamat',
+															'$v_lulusan',
+															'$v_usia',
+															'$v_no_hp',
+															'$v_pengalaman',
+															'$v_email',
+															'$v_tinggi_badan',
+															'$v_foto',
+															'$v_kelebihan')
+															";
+					if ( $con->query($sql) === TRUE ) {
+						echo "berhasil simpan";
+					} else {
+						echo "terjadi kesalahan fatal" .$sql.' <br> ' .$conn->error;
+					}
+				} else {
+					echo 'gagal upload file';
+				}
+			} else {
+				echo 'ukuran file maks 1 mb';
+			}
+		} else {
+			echo 'ekstensi file tidak diijinkan';
+		}
 
 	}
 ?>
