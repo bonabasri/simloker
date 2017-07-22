f0782853eeaabacef95931b07d4e48a0b3fb7f8e
luqmanhakim1 <luckman.heckem@gmail.com>
2017-07-22 15:28:05 +0700

    tambah validasi upload ijazah

--- modul/apply-save.php
+++ modul/apply-save.php
@@ -5,6 +5,7 @@ if (isset($_POST['apply-save']))
 	$v_user_id 		= $_SESSION['user_id'];
 
 	$allowExt 			= array( 'doc', 'docx', 'pdf' );
+	$allowExt1 			= array( 'pdf' );
 
 	$fileName 			= $_FILES['file']['name'];
 	$fileExt			= strtolower(end(explode('.', $fileName)));
@@ -25,42 +26,55 @@ if (isset($_POST['apply-save']))
 	$v_files 			= str_replace(' ','_',$files);
 
 	if ( in_array( $fileExt, $allowExt ) === TRUE ) 
+	{
+		if ( in_array( $fileExt1, $allowExt1 ) === TRUE ) 
 		{
 			if ( $fileSize < 1044070 ) 
 			{
-				$sqlcek = "SELECT * FROM tb_lamaran WHERE id_lowongan = '".$v_id_lowongan."' AND user_id = '".$v_user_id."' ";
-		    	$result = $conn->query($sqlcek);
+				if ( $fileSize1 < 1044070 ) 
+				{
+					$sqlcek = "SELECT * FROM tb_lamaran WHERE id_lowongan = '".$v_id_lowongan."' AND user_id = '".$v_user_id."' ";
+		    		$result = $conn->query($sqlcek);
 
-			    if ($result->num_rows > 0) {
-			        echo '<script>alert("anda sudah mengirim lamaran"); </script>';
-			        echo '<meta http-equiv="refresh" content="0;URL=?p=home">';
-			    } else {
+				    if ($result->num_rows > 0) {
+				        echo '<script>alert("anda sudah mengirim lamaran"); </script>';
+				        echo '<meta http-equiv="refresh" content="0;URL=?p=home">';
+				    } else {
 
-					if ( move_uploaded_file( $fileTemp,$upload_dir.$v_file ) && move_uploaded_file( $fileTemp1,$upload_dir1.$v_files ) )
-					{
-						$sql = "INSERT INTO tb_lamaran (id_lowongan,user_id,file,files,tgl_lamar)
+						if ( move_uploaded_file( $fileTemp,$upload_dir.$v_file ) && move_uploaded_file( $fileTemp1,$upload_dir1.$v_files ) )
+						{
+						$sql = "INSERT INTO tb_lamaran 
+										(id_lowongan,user_id,file,files,tgl_lamar)
 								VALUES ('$v_id_lowongan','$v_user_id','$v_file','$v_files',NOW())";
 
-						if ( $conn->query($sql) === TRUE ) {
-							echo '<script>alert("lamaran berhasil dikirim"); </script>';
-				 			echo '<meta http-equiv="refresh" content="0;URL=?p=home">';
+							if ( $conn->query($sql) === TRUE ) {
+								echo '<script>alert("lamaran berhasil dikirim"); </script>';
+					 			echo '<meta http-equiv="refresh" content="0;URL=?p=home">';
+							} else {
+							// echo "terjadi kesalahan fatal" .$sql.' <br> ' .$conn->error;
+							echo '<script>alert("data gagal disimpan disimpan"); </script>';
+					 		echo '<meta http-equiv="refresh" content="0;URL=?p=home">';
+							}
 						} else {
-						// echo "terjadi kesalahan fatal" .$sql.' <br> ' .$conn->error;
-						echo '<script>alert("data gagal disimpan disimpan"); </script>';
-				 		echo '<meta http-equiv="refresh" content="0;URL=?p=apply">';
+							echo '<script>alert("gagal upload file"); </script>';
+							echo '<meta http-equiv="refresh" content="0;URL=?p=home">';
 						}
-					} else {
-						echo '<script>alert("gagal upload file"); </script>';
-						echo '<meta http-equiv="refresh" content="0;URL=?p=apply">';
 					}
+				} else {
+					echo '<script>alert("ukuran file ijazah maks 1 mb"); </script>';
+					echo '<meta http-equiv="refresh" content="0;URL=?p=home">';
 				}
 			} else {
-				echo '<script>alert("ukuran file maks 1 mb"); </script>';
-				echo '<meta http-equiv="refresh" content="0;URL=?p=apply">';
+				echo '<script>alert("ukuran file cv maks 1 mb"); </script>';
+				echo '<meta http-equiv="refresh" content="0;URL=?p=home">';
 			}
 		} else {
-			echo '<script>alert("ekstensi file tidak diijinkan"); </script>';
-			echo '<meta http-equiv="refresh" content="0;URL=?p=apply&id=<?php echo $data["id_lowongan"]; ?>">';
+			echo '<script>alert("ekstensi file ijazah harus pdf"); </script>';
+			echo '<meta http-equiv="refresh" content="0;URL=?p=home">';
 		}
-		$conn->close();
+	} else {
+		echo '<script>alert("ekstensi file cv tidak diijinkan"); </script>';
+		echo '<meta http-equiv="refresh" content="0;URL=?p=home">';
 	}
+	$conn->close();
+}
